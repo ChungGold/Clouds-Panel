b67c978a4c45027e3457e72a434b0616b1fdcae0
ChungGold <74089047+ChungGold@users.noreply.github.com>
2021-02-02 18:30:50 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const panels = document.getElementsByClassName('panels');
 
 for (let i = 0; i < panels.length; i++) {
-    panels[i].addEventListener('click', function() {
+    panels[i].addEventListener('mouseleave', function() {
         this.classList.add('face'+ i);
-        this.style.position ='absolute';
+        this.style.position = 'absolute';
     });
 }
 
 const cubes = document.getElementsByClassName('cubes');
 
 for (let i = 0; i < cubes.length; i++) {
-    cubes[i].addEventListener('mouseleave', function() {
+    cubes[i].addEventListener('click', function() {
         this.classList.add('cubes'+ i);
     });
 }
+
+const whole = document.getElementById('img-container');
+
+whole.addEventListener('click', function() {
+    whole.classList.add('rotateMore');
+    console.log(1);
+}); 
